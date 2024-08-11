f766b10dd8c14dca5d51cc9cdd7e144834dff4bd
Narasimha <ngonapa@cisco.com>
2024-08-11 15:16:03 +0530

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -6,9 +6,9 @@ Set-Location -Path $PSScriptRoot
 
 # Set variables
 $scriptDir = Get-Location
-$phpSdkDir = Join-Path -Path $scriptDir -ChildPath "php-sdk-binary-tools-master"
-$phpSourceDir = Join-Path -Path $phpSdkDir -ChildPath "phpdev\vc16\x64\php-8.2.21-src"
-$extDir = Join-Path -Path $phpSourceDir -ChildPath "ext\php-agent"
+$phpSdkDir = "C:\\windows_test7\\php-sdk-binary-tools-master"
+$phpSourceDir = "C:\\windows_test7\\php-sdk-binary-tools-master\\phpdev\\vc16\\x64\\php-8.2.21-src"
+
 
 # Copy PHP agent to the extensions folder
 Set-Location -Path $phpSdkDir
@@ -20,11 +20,21 @@ Set-Location -Path $scriptDir
 # Copy the PHP source files
 Copy-Item -Path "php-8.2.21-src" -Destination "$phpSdkDir\phpdev\vc16\x64\php-8.2.21-src" -Recurse -Force
 
+Set-Location -Path $phpSdkDir
+
+# Print the current directory for debugging
+Write-Output "Current directory: $(Get-Location)"
+
+# List all directories in the current location for debugging
+Write-Output "Directories in current location:"
+Get-ChildItem -Directory
+
+
 # Execute the batch file and wait for it to complete
 Start-Process -FilePath "C:\\windows_test6\php-sdk-binary-tools-master\\phpsdk-vs16-x64.bat" -Wait -NoNewWindow
 
 # Change directory to the specified path
-Set-Location "C:\windows_test6\php-sdk-binary-tools-master\phpdev\vc16\x64\php-8.2.21-src"
+Set-Location "C:\windows_test7\php-sdk-binary-tools-master\phpdev\vc16\x64\php-8.2.21-src"
 
 # Run the subsequent commands
 & .\buildconf --force
