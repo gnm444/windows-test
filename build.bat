0e0c8336b4f4cc050efb951eda8f58e9c3043597
Narasimha <ngonapa@cisco.com>
2024-08-11 16:23:06 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,8 +7,8 @@ REM Navigate to the directory containing the bat script.
 cd /d "%~dp0"
 
 REM Set variables
-set "scriptDir=C:\\windows-test9"
-set "phpSdkDir=C:\\windows-test9\\php-sdk-binary-tools-master"
+set "scriptDir=C:\\windows-test10"
+set "phpSdkDir=C:\\windows-test10\\php-sdk-binary-tools-master"
 set "phpSourceDir=%phpSdkDir%\\phpdev\\vc16\\x64\\php-8.2.21-src"
 set "extDir=%phpSourceDir%\\ext\\php-agent"
 
@@ -21,9 +21,9 @@ cd /d "%scriptDir%"
 
 
 xcopy /e /i /y php-8.2.21-src "%phpSdkDir%\\phpdev\\vc16\\x64\\php-8.2.21-src"
-cd /d "C:\\windows-test9\\php-sdk-binary-tools-master"
+cd /d "C:\\windows-test10\\php-sdk-binary-tools-master"
 @echo off
-call "C:\\windows-test9\\php-sdk-binary-tools-master\\phpsdk-starter.bat" -c vc16 -a x64 -t "C:\\windows-test9\\php-sdk-binary-tools-master\\build-task.bat" --task-args "clean build"
+call "C:\\windows-test10\\php-sdk-binary-tools-master\\phpsdk-starter.bat" -c vc16 -a x64 -t "C:\\windows-test10\\php-sdk-binary-tools-master\\build-task.bat" --task-args "clean build"
 
 
 REM Output
