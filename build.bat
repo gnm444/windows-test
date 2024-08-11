8b260bc67ed477852ffbe0e486d9415b541d01ea
Narasimha <ngonapa@cisco.com>
2024-08-11 14:57:12 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,24 +7,24 @@ REM Navigate to the directory containing the bat script.
 cd /d "%~dp0"
 
 REM Set variables
-set "scriptDir=C:\windows_test6"
-set "phpSdkDir=C:\windows_test6\php-sdk-binary-tools-master"
-set "phpSourceDir=%phpSdkDir%\phpdev\vc16\x64\php-8.2.21-src"
-set "extDir=%phpSourceDir%\ext\php-agent"
+set "scriptDir=C:\\windows_test6"
+set "phpSdkDir=C:\\windows_test6\\php-sdk-binary-tools-master"
+set "phpSourceDir=%phpSdkDir%\\phpdev\\vc16\\x64\\php-8.2.21-src"
+set "extDir=%phpSourceDir%\\ext\\php-agent"
 
 REM Copy PHP agent to the extensions folder
 cd /d "%phpSdkDir%"
-call bin\phpsdk_setvars.bat
-call bin\phpsdk_buildtree.bat phpdev
+call bin\\phpsdk_setvars.bat
+call bin\\phpsdk_buildtree.bat phpdev
 
 cd /d "%scriptDir%"
 
 
-xcopy /e /i /y php-8.2.21-src "%phpSdkDir%\phpdev\vc16\x64\php-8.2.21-src"
-cd /d "C:\windows_test6\php-sdk-binary-tools-master"
+xcopy /e /i /y php-8.2.21-src "%phpSdkDir%\\phpdev\\vc16\\x64\\php-8.2.21-src"
+cd /d "C:\\windows_test6\\php-sdk-binary-tools-master"
 @echo off
 call phpsdk-vs16-x64.bat
-cd /d "C:\windows_test6\php-sdk-binary-tools-master\phpdev\vc16\x64\php-8.2.21-src"
+cd /d "C:\\windows_test6\\php-sdk-binary-tools-master\\phpdev\\vc16\\x64\\php-8.2.21-src"
 buildconf --force
 configure --disable-all --enable-cli
 nmake
