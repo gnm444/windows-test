22d460fa19cf03299e0465aa31a67dad54a8ea15
Narasimha <ngonapa@cisco.com>
2024-08-12 05:22:49 +0530

    Update build-task.bat

--- build-task.bat
+++ build-task.bat
@@ -1,5 +1,5 @@
 REM Running Build Task
-cd /d "C:\\windows-test11\\php-sdk-binary-tools-master\\phpdev\\vc16\\x64\\php-8.2.21-src"
+cd /d "C:\\windows-test13\\php-sdk-binary-tools-master\\phpdev\\vc16\\x64\\php-8.2.21-src"
 buildconf --force
 configure --disable-all --enable-cli
 nmake
