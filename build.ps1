639afc83e857c6999c1c43187c0c9a2a5d8ab06c
Narasimha <ngonapa@cisco.com>
2024-08-11 14:11:57 +0530

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -20,7 +20,16 @@ Set-Location -Path $scriptDir
 # Copy the PHP source files
 Copy-Item -Path "php-8.2.21-src" -Destination "$phpSdkDir\phpdev\vc16\x64\php-8.2.21-src" -Recurse -Force
 Set-Location -Path $phpSdkDir
-& .\phpsdk-vs16-x64.bat
+# Execute the batch file and wait for it to complete
+Start-Process -FilePath "phpsdk-vs16-x64.bat" -Wait -NoNewWindow
+
+# Change directory to the specified path
+Set-Location "C:\windows_test4\php-sdk-binary-tools-master\phpdev\vc16\x64\php-8.2.21-src"
+
+# Run the subsequent commands
+& .\buildconf --force
+& .\configure --disable-all --enable-cli
+& nmake
 
 # Output
 Write-Output "Build complete: $phpSourceDir\x64\Release_TS\php_agent.dll"
